371bc5bd2c4f14930c0fab6dbd27708571ebc25d
Simon Wong <Simon.Wong@heartlandgeneration.com>
2023-10-30 14:41:18 -0600

    Added NERC Holiday List.  NERC only recognizes 6 holidays

---
+++ src/PublicHoliday/NERCHoliday.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PublicHoliday
+{
+    /// <summary>
+    /// NERC Holidays in the US
+    /// <br />
+    /// If a holiday falls on a Saturday the NERC is open the preceding Friday, this differs from how <see cref="USAPublicHoliday"></see> treats Fridays preceding a Saturday holiday.
+    /// <br />
+    /// If a holiday falls on a Sunday the NERC is closed following Monday.
+    /// https://www.nerc.com/Pages/default.aspx
+    /// </summary>
+    public class NERCHoliday : PublicHolidayBase
+    {
+        #region Holiday Adjustments
+        private static DateTime FixWeekend(DateTime hol)
+        {
+            if (hol.DayOfWeek == DayOfWeek.Sunday)
+                hol = hol.AddDays(1);
+            return hol;
+        }
+        #endregion
+
+        #region Individual Holidays
+
+        /// <summary>
+        /// New Years Day. Note in 1999 and 2005 it was 31st December
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        public static Holiday NewYear(int year)
+        {
+            var holiday = new DateTime(year, 1, 1);
+            return new Holiday(holiday, FixWeekend(holiday), "NewYear");
+        }
+
+
+        /// <summary>
+        /// Last Monday in May
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        public static Holiday MemorialDay(int year)
+        {
+            var hol = new DateTime(year, 5, 25);
+            hol = HolidayCalculator.FindFirstMonday(hol);
+            return new Holiday(hol, hol, "MemorialDay");
+        }
+
+
+        /// <summary>
+        /// Independence Day
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        public static Holiday IndependenceDay(int year)
+        {
+            var hol = new DateTime(year, 7, 4);
+            var observed = FixWeekend(hol);
+            return new Holiday(hol, observed, "IndependenceDay");
+        }
+
+        /// <summary>
+        /// First Monday in September
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        public static Holiday LaborDay(int year)
+        {
+            var hol = new DateTime(year, 9, 1);
+            hol = HolidayCalculator.FindFirstMonday(hol);
+            return new Holiday(hol, hol, "LaborDay");
+        }
+         
+        /// <summary>
+        /// Thanksgiving - Fourth Thursday in November
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        public static Holiday Thanksgiving(int year)
+        {
+            var hol = new DateTime(year, 11, 22);
+            hol = HolidayCalculator.FindOccurrenceOfDayOfWeek(hol, DayOfWeek.Thursday, 1);
+            return new Holiday(hol, hol, "Thanksgiving");
+        }
+
+        /// <summary>
+        /// Christmas Day
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        public static Holiday Christmas(int year)
+        {
+            var hol = new DateTime(year, 12, 25);
+            return new Holiday(hol, FixWeekend(hol), "Christmas");
+        }
+        #endregion
+
+        /// <summary>
+        /// Get a list of dates for all holidays in a year.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        public override IList<DateTime> PublicHolidays(int year)
+        {
+            var bHols = new List<DateTime>();
+            bHols.Add(NewYear(year)); //1st January
+            bHols.Add(MemorialDay(year)); //Last Monday in May
+
+            bHols.Add(IndependenceDay(year)); //4 July
+            bHols.Add(LaborDay(year)); //First Monday in September
+
+            bHols.Add(Thanksgiving(year)); //Fourth Thursday in November
+            bHols.Add(Christmas(year)); //25 December
+
+            return bHols;
+        }
+
+        /// <summary>
+        /// Gets a list of public holidays with their observed and actual date
+        /// </summary>
+        /// <param name="year">The given year</param>
+        /// <returns></returns>
+        public override IList<Holiday> PublicHolidaysInformation(int year)
+        {
+            var bHols = new List<Holiday>();
+            bHols.Add(NewYear(year)); //1st January
+            bHols.Add(MemorialDay(year)); //Last Monday in May
+
+            bHols.Add(IndependenceDay(year)); //4 July
+            bHols.Add(LaborDay(year)); //First Monday in September
+            bHols.Add(Thanksgiving(year)); //Fourth Thursday in November
+            bHols.Add(Christmas(year)); //25 December
+            return bHols;
+        }
+
+        /// <summary>
+        /// Get a list of dates with names for all holidays in a year.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        public override IDictionary<DateTime, string> PublicHolidayNames(int year)
+        {
+            var bHols = new Dictionary<DateTime, string>();
+            bHols.Add(NewYear(year), "New Year"); //1st January
+            bHols.Add(MemorialDay(year), "Memorial Day"); //Last Monday in May
+            bHols.Add(IndependenceDay(year), "Independence Day"); //4 July
+            bHols.Add(LaborDay(year), "Labor Day"); //First Monday in September
+            bHols.Add(Thanksgiving(year), "Thanksgiving"); //Fourth Thursday in November
+            bHols.Add(Christmas(year), "Christmas"); //25 December
+            return bHols;
+        }
+        /// <summary>
+        /// Check if a specific date is a federal holiday.
+        /// Obviously the PublicHoliday list is more efficient for repeated checks
+        /// </summary>
+        /// <param name="dt">The date you wish to check</param>
+        /// <returns>True if date is a federal holiday (excluding weekends)</returns>
+        public override bool IsPublicHoliday(DateTime dt)
+        {
+            int year = dt.Year;
+            var date = dt.Date;
+
+            switch (dt.Month)
+            {
+                case 1:
+                    if (NewYear(year) == date)
+                        return true;
+                    break;
+                case 5:
+                    if (MemorialDay(year) == date)
+                        return true;
+                    break;
+                case 7:
+                    if (IndependenceDay(year) == date)
+                        return true;
+                    break;
+                case 9:
+                    if (LaborDay(year) == date)
+                        return true;
+                    break;
+                case 11:
+                    if (Thanksgiving(year) == date)
+                        return true;
+                    break;
+                case 12:
+                    if (Christmas(year) == date)
+                        return true;
+                    if (NewYear(year + 1) == date)
+                        return true; //31st December if New Year is Saturday
+                    break;
+            }
+            return false;
+        }
+    }
+}
---
+++ tests/PublicHolidayTests/TestNERCHoliday.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PublicHoliday;
+
+namespace PublicHolidayTests
+{
+
+    /// <summary>
+    /// Using official calendar from http://www.opm.gov/fedhol/2006.asp
+    /// </summary>
+    [TestClass]
+    public class TestNERCHoliday
+    {
+        [TestMethod]
+        public void TestThanksgiving2018()
+        {
+            var expected = new DateTime(2018, 11, 22);
+            var actual = NERCHoliday.Thanksgiving(2018);
+            Assert.AreEqual(expected, actual);
+        }
+
+         [TestMethod]
+        public void TestNewYear2004()
+        {
+            var expected = new DateTime(2004, 1, 1);
+            var actual = NERCHoliday.NewYear(2004);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void TestMemorial2004()
+        {
+            var expected = new DateTime(2004, 5, 31);
+            var actual = NERCHoliday.MemorialDay(2004);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestIndependence2004()
+        {
+            var expected = new DateTime(2004, 7, 5);
+            var actual = NERCHoliday.IndependenceDay(2004);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void TestLabor2004()
+        {
+            var expected = new DateTime(2004, 9, 6);
+            var actual = NERCHoliday.LaborDay(2004);
+            Assert.AreEqual(expected, actual);
+        }
+
+          [TestMethod]
+        public void TestThanksgiving2004()
+        {
+            var expected = new DateTime(2004, 11, 25);
+            var actual = NERCHoliday.Thanksgiving(2004);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestChristmas2004()
+        {
+            var expected = new DateTime(2004, 12, 25);
+            var actual = NERCHoliday.Christmas(2004);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestNewYear()
+        {
+            var expected = new DateTime(2006, 1, 2);
+            var actual = NERCHoliday.NewYear(2006);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void TestMemorial()
+        {
+            var expected = new DateTime(2006, 5, 29);
+            var actual = NERCHoliday.MemorialDay(2006);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestIndependence()
+        {
+            var expected = new DateTime(2006, 7, 4);
+            var actual = NERCHoliday.IndependenceDay(2006);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void TestThanksgiving()
+        {
+            var expected = new DateTime(2006, 11, 23);
+            var actual = NERCHoliday.Thanksgiving(2006);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestChristmas()
+        {
+            var expected = new DateTime(2006, 12, 25);
+            var actual = NERCHoliday.Christmas(2006);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestThanksgiving1999()
+        {
+            var expected = new DateTime(1999, 11, 25);
+            var actual = NERCHoliday.Thanksgiving(1999);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestIsPublicHoliday()
+        {
+            var thanksgiving = new DateTime(1999, 11, 25);
+            var result = new NERCHoliday().IsPublicHoliday(thanksgiving);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestNextWorkingDay()
+        {
+            var thanksgiving = new DateTime(1999, 11, 25);
+            var result = new NERCHoliday().NextWorkingDay(thanksgiving);
+            Assert.AreEqual(new DateTime(1999, 11, 26), result);
+        }
+
+     
+        [TestMethod]
+        public void TestPublicHolidays()
+        {
+            var thanksgiving = new DateTime(1999, 11, 25);
+            var result = new NERCHoliday().PublicHolidays(1999);
+            Assert.AreEqual(6, result.Count);
+            Assert.IsTrue(result.Contains(thanksgiving));
+        }
+
+        [TestMethod]
+        public void TestPublicHolidayInformation()
+        {
+            var nercHoliday = new NERCHoliday();
+            var hols = nercHoliday.PublicHolidays(2017);
+            var infos = nercHoliday.PublicHolidaysInformation(2017);
+            Assert.AreEqual(hols.Count, infos.Count);
+            foreach (var info in infos)
+            {
+                Assert.IsTrue(hols.Contains(info), "observed date is implicitly in both lists");
+            }
+        }
+
+        #region 2022 Tests
+
+        [TestMethod]
+        public void TestNewYears2022()
+        {
+            var expected = new DateTime(2022, 1, 1);
+            var actual = NERCHoliday.NewYear(2022);
+            Assert.AreEqual(expected, actual);
+        }
+
+ 
+        [TestMethod]
+        public void TestMemorialDay2022()
+        {
+            var expected = new DateTime(2022, 5, 30);
+            var actual = NERCHoliday.MemorialDay(2022);
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+
+        [TestMethod]
+        public void TestIndependenceDay2022()
+        {
+            var expected = new DateTime(2022, 7, 4);
+            var actual = NERCHoliday.IndependenceDay(2022);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestLaborDay2022()
+        {
+            var expected = new DateTime(2022, 9, 5);
+            var actual = NERCHoliday.LaborDay(2022);
+            Assert.AreEqual(expected, actual);
+        }
+
+ 
+ 
+        [TestMethod]
+        public void TestThanksgiving2022()
+        {
+            var expected = new DateTime(2022, 11, 24);
+            var actual = NERCHoliday.Thanksgiving(2022);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestChristmas2022()
+        {
+            var expected = new DateTime(2022, 12, 26);
+            var actual = NERCHoliday.Christmas(2022);
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+
+        #region 2023 Tests
+
+        [TestMethod]
+        public void TestNewYears2023()
+        {
+            var expected = new DateTime(2023, 1, 2);
+            var actual = NERCHoliday.NewYear(2023);
+            Assert.AreEqual(expected, actual);
+        }
+
+ 
+ 
+
+        [TestMethod]
+        public void TestMemorialDay2023()
+        {
+            var expected = new DateTime(2023, 5, 29);
+            var actual = NERCHoliday.MemorialDay(2023);
+            Assert.AreEqual(expected, actual);
+        }
+
+ 
+
+        [TestMethod]
+        public void TestIndependenceDay2023()
+        {
+            var expected = new DateTime(2023, 7, 4);
+            var actual = NERCHoliday.IndependenceDay(2023);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestLaborDay2023()
+        {
+            var expected = new DateTime(2023, 9, 4);
+            var actual = NERCHoliday.LaborDay(2023);
+            Assert.AreEqual(expected, actual);
+        }
+
+  
+
+        [TestMethod]
+        public void TestThanksgiving2023()
+        {
+            var expected = new DateTime(2023, 11, 23);
+            var actual = NERCHoliday.Thanksgiving(2023);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestChristmas2023()
+        {
+            var expected = new DateTime(2023, 12, 25);
+            var actual = NERCHoliday.Christmas(2023);
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+    }
+}
